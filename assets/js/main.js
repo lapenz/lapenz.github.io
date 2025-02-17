81518916ece1d27324a13f67e7e0f8e7ec054712
lucaspenz <lucasarthurpenz@gmail.com>
2025-02-17 10:45:28 -0500

    Fix GitHub plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,12 +62,5 @@ jQuery(document).ready(function($) {
         }
     );
     
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "lapenz", {responsive: true});
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "lapenz", selector: "#ghfeed" });
-
 
 });
